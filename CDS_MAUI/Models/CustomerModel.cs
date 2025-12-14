22b68912b709c9675b838c0138d7a981ed8f5202
Delus10n7 <delusionbeat@gmail.com)>
2025-12-14 19:35:39 +0300

    Изменение CarOrderModal

---
+++ CDS_MAUI/Models/CustomerModel.cs
@@ -0,0 +1,33 @@
+﻿using CDS_Interfaces.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CDS_MAUI.Models
+{
+    public class CustomerModel
+    {
+        public int? Id { get; set; }
+        public string? FullName { get; set; }
+        public string? Phone { get; set; }
+        public string? Email { get; set; }
+
+        public CustomerModel(CustomerDTO c)
+        {
+            Id = c.Id;
+            FullName = c.FullName;
+            Phone = c.PhoneNumber;
+            Email = c.Email;
+        }
+
+        public CustomerModel(string FullName, string Phone, string Email, int Id = 0)
+        {
+            this.Id = Id;
+            this.FullName = FullName;
+            this.Phone = Phone;
+            this.Email = Email;
+        }
+    }
+}
--- CDS_MAUI/ViewModels/CarsVM/CarOrderViewModel.cs
+++ CDS_MAUI/ViewModels/CarsVM/CarOrderViewModel.cs
@@ -1,10 +1,12 @@
-﻿using CDS_Interfaces.Service;
+﻿using CDS_DomainModel.Entities;
 using CDS_Interfaces.DTO;
+using CDS_Interfaces.Service;
 using CDS_MAUI.Models;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,19 +19,37 @@ namespace CDS_MAUI.ViewModels.CarsVM
         private CarModel _car;
 
         [ObservableProperty]
-        private List<string> _managers = new();
+        private ObservableCollection<string> _managers = new();
+
+        [ObservableProperty]
+        private ObservableCollection<string> _customers = new();
+
+        [ObservableProperty]
+        private ObservableCollection<CustomerModel> _filteredCustomers = new();
 
         [ObservableProperty]
         private string _selectedManager;
 
         [ObservableProperty]
-        private string _customerName;
+        private string _selectedCustomer;
 
         [ObservableProperty]
-        private string _customerPhone;
+        private string _customerName = "";
 
         [ObservableProperty]
-        private string _customerEmail;
+        private string _customerPhone = "";
+
+        [ObservableProperty]
+        private string _customerEmail = "";
+
+        [ObservableProperty]
+        private bool _newCustomer = false;
+
+        [ObservableProperty]
+        private bool _oldCustomer = false;
+
+        [ObservableProperty]
+        private string _searchText = "";
 
         // === СЕРВИСЫ ===
         ICarService _carService;
@@ -45,13 +65,18 @@ namespace CDS_MAUI.ViewModels.CarsVM
             _userService = userService;
 
             Title = "Оформление заказа";
-            InitializeManagers();
+            InitializeManagersAndCustomers();
         }
 
-        public void InitializeManagers()
+        public void InitializeManagersAndCustomers()
         {
+            Customers.Clear();
+            Customers.Add("Не выбран");
+            SelectedCustomer = Customers[0];
+
             Managers.Clear();
             Managers.Add("Не выбран");
+            SelectedManager = Managers[0];
 
             List<ManagerDTO> managerDTOs = _userService.GetAllManagers();
 
@@ -72,6 +97,82 @@ namespace CDS_MAUI.ViewModels.CarsVM
         {
             await Shell.Current.Navigation.PopModalAsync();
             await Shell.Current.Navigation.PopModalAsync();
+        }
+
+        [RelayCommand]
+        private void ShowNewCustomerMenu()
+        {
+            NewCustomer = !NewCustomer;
+            OldCustomer = false;
+        }
+
+        [RelayCommand]
+        private void ShowOldCustomerMenu()
+        {
+            NewCustomer = false;
+            OldCustomer = !OldCustomer;
+        }
+
+        [RelayCommand]
+        private void Search()
+        {
+            List<CustomerDTO> customerDTOs = _userService.GetAllCustomers();
+
+            List<CustomerDTO> filtered = new List<CustomerDTO>();
+
+            if (!string.IsNullOrEmpty(SearchText))
+            {
+                var searchLower = SearchText.ToLower();
+                foreach (var customer in customerDTOs)
+                {
+                    filtered = customerDTOs.Where(c => 
+                        c.FullName.ToLower().Contains(searchLower) ||
+                        c.PhoneNumber.ToLower().Contains(searchLower) ||
+                        c.Email.ToLower().Contains(searchLower)
+                    ).ToList();
+                }
+
+                FilteredCustomers.Clear();
+                foreach (var customer in filtered)
+                {
+                    FilteredCustomers.Add(new CustomerModel(customer));
+                }
+            }
+            else
+            {
+                FilteredCustomers.Clear();
+            }
+        }
+
+        [RelayCommand]
+        private void SelectCustomer(CustomerModel customer)
+        {
+            NewCustomer = false;
+            OldCustomer = false;
+
+            if (customer == null) return;
+
+            Customers.Clear();
+            Customers.Add("Не выбран");
+            Customers.Add(customer.FullName);
+
+            SelectedCustomer = customer.FullName;
+        }
+
+        [RelayCommand]
+        private void SelectNewCustomer()
+        {
+            NewCustomer = false;
+            OldCustomer = false;
+
+            if (!string.IsNullOrEmpty(CustomerName) && !string.IsNullOrEmpty(CustomerPhone) && !string.IsNullOrEmpty(CustomerEmail))
+            {
+                Customers.Clear();
+                Customers.Add("Не выбран");
+                Customers.Add(CustomerName);
+
+                SelectedCustomer = CustomerName;
+            }
         }
     }
 }
